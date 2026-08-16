5cacb9f1a00dc1646303ba23696dcaae8be6da8c
zhoumingzhe555-cloud <zhoumingzhe555@gmail.com>
2026-08-16 18:42:07 +0800

    Update customer_db.py

--- core/customer_db.py
+++ core/customer_db.py
@@ -1,35 +1,68 @@
 import sqlite3
+from datetime import datetime
+from config import DATABASE_PATH
 
-def save_customer(db_path, customer):
-    conn=sqlite3.connect(db_path)
-    c=conn.cursor()
 
-    c.execute('''
+def save_customer(data, submitter="", chat_id=""):
+
+    conn = sqlite3.connect(DATABASE_PATH)
+
+    c = conn.cursor()
+
+
+    c.execute("""
     CREATE TABLE IF NOT EXISTS customers(
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+
         name TEXT,
         age TEXT,
         job TEXT,
         income TEXT,
         work_year TEXT,
         software TEXT,
-        receiver TEXT
-    )
-    ''')
+        receiver TEXT,
 
-    c.execute('''
+        submitter TEXT,
+        chat_id TEXT,
+
+        created_time TEXT
+    )
+    """)
+
+
+    c.execute("""
     INSERT INTO customers
-    (name,age,job,income,work_year,software,receiver)
-    VALUES(?,?,?,?,?,?,?)
-    ''',(
-        customer['name'],
-        customer['age'],
-        customer['job'],
-        customer['income'],
-        customer['work_year'],
-        customer['software'],
-        customer['receiver']
+    (
+        name,
+        age,
+        job,
+        income,
+        work_year,
+        software,
+        receiver,
+        submitter,
+        chat_id,
+        created_time
+    )
+
+    VALUES(?,?,?,?,?,?,?,?,?,?)
+
+    """,
+    (
+        data.get("name"),
+        data.get("age"),
+        data.get("job"),
+        data.get("income"),
+        data.get("work_year"),
+        data.get("software"),
+        data.get("receiver"),
+        submitter,
+        chat_id,
+        datetime.now().isoformat()
     ))
 
+
     conn.commit()
+
     conn.close()
